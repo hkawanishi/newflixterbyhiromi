e0808d256ac1d7a0b39a1d76c43dfc8a9fb2e853
Hiromi Kawanishi <contacthiromi@gmail.com>
2015-11-26 01:30:53 +0000

    install the devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Flixter2::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.default_url_options = { :host => 'flixter-hiromi.herokuapp.com' }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
