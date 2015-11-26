54f0da9c3f0b44f69cd49ac0edb2047633bea00b
Hiromi Kawanishi <contacthiromi@gmail.com>
2015-11-26 23:06:36 +0000

    list out courses

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -4,4 +4,22 @@ class CourseTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "title should be present" do
+    course = FactoryGirl.create(:course)
+    course.title = " "
+    assert_not course.valid?
+  end
+
+  test "description should be present" do
+    course = FactoryGirl.create(:course)
+    course.description = " "
+    assert_not course.valid?
+  end
+
+  test "cost should be present" do
+    course = FactoryGirl.create(:course)
+    course.cost = nil
+    assert_not course.valid?
+  end
+
 end
