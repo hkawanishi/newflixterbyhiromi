10ecd9d7abedf72becbee5d71e32c126707e41aa
Hiromi Kawanishi <contacthiromi@gmail.com>
2015-11-27 01:41:16 +0000

    set up the student course show page

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -4,4 +4,15 @@ class CoursesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should get courses index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get courses show" do
+    course = FactoryGirl.create(:course)
+    get :show, :id => course.id
+    assert_response :success
+  end
+
 end
