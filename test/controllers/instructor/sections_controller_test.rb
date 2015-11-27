a5a3defef666048a3ae67a061ff077754d13e86d
Hiromi Kawanishi <contacthiromi@gmail.com>
2015-11-27 02:43:27 +0000

    add section test

--- test/controllers/instructor/sections_controller_test.rb
+++ test/controllers/instructor/sections_controller_test.rb
@@ -4,4 +4,24 @@ class Instructor::SectionsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "section new found" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+    course = FactoryGirl.create(:course)
+    get :new, :course_id => course.id
+    assert_response :success
+  end
+
+  test "section create found" do
+    user = FactoryGirl.create(:user)
+    sign_in user
+    course = FactoryGirl.create(:course)
+    assert_difference 'Section.count' do
+      post :create, :course_id => course.id, :section => {
+        :title => 'sometitle'
+      }
+
+    end
+    assert_equal 1, course.sections.count
+  end
 end
